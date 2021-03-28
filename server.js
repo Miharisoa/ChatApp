a7f626f9b194d837989e62311eea598b7d933702
Mihary <raharitsiresy@gmail.com>
2021-03-28 12:14:32 +0300

    Modif back

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 const PORT = 3001;
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
 app.get('/', (req, res) => res.send('hello!'));
 http.listen(PORT, () => {
-    console.log('listening on *:3000');
+    console.log('listening on *:3001');
 });
